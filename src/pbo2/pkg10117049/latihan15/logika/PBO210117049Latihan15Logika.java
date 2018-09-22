7a775161606d6370ae610ef25810ef2838b6c155
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-09-22 16:33:53 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan15/logika/PBO210117049Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan15.logika;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama                 : Gery Akbar Fauzi
+ * Kelas                : PBO2
+ * Nim                  : 10117049
+ * Deskripsi Program    : Program ini berisi program operasi boolean dengan
+ *                        operator logika
+ */
+public class PBO210117049Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+ (a && b));
+        System.out.println("a || b = "+ (a || b));
+        System.out.println("!a = "+ !a);
+        System.out.println("!(a && b) = "+ !(a && b));
+    }
+    
+}
